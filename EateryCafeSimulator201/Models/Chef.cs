6f2763ff9629dad89a24a2924bb0debd4cf7ea84
nihatcff <nihatrkh-mpa101@code.edu.az>
2026-01-19 14:57:38 +0400

    Add project files.

---
+++ EateryCafeSimulator201/Models/Chef.cs
@@ -0,0 +1,9 @@
+﻿using EateryCafeSimulator201.Models.Common;
+
+namespace EateryCafeSimulator201.Models
+{
+    public class Chef:BaseEntity
+    {
+       
+    }
+}
---
+++ EateryCafeSimulator201/Models/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace EateryCafeSimulator201.Models.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
